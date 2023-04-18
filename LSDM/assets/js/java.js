7e114c6959d1aef1046d9abce242c424d0d54060
jenellemillison <JAMillison@comcast.net>
2023-04-18 07:11:26 -0500

    dropdown must be AFTER other function or it gives errors

--- LSDM/assets/js/java.js
+++ LSDM/assets/js/java.js
@@ -1,38 +1,3 @@
-/*
-	Dropdown with the search info
-*/
-function dropdown(){
-	document.getElementById("topic-drop-search").classList.toggle("show");
-}
-
-
-/*
-	Take input from the search bar and update the list shown for topic selection
-	From https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_filter
-*/
-function searchMatch() {
-  var input;
-  var inputUpperCase;
-  var topics;
-  var txtValue;
-  var btn;
-  var i;
-  input = document.getElementById("topic-search");
-  inputUpperCase = input.value.toUpperCase();
-  topics = document.getElementById("topic-drop-search");
-  btn = topics.getElementsByTagName("button");
-  for (i = 0; i < btn.length; i++) {
-    txtValue = btn[i].innerHTML;
-    if (txtValue.toUpperCase().indexOf(inputUpperCase) > -1) {
-      btn[i].style.display = "";
-    } else {
-      btn[i].style.display = "none";
-    }
-  }
-}
-
-
-
 
 
 /**
@@ -272,4 +237,37 @@ function searchMatch() {
 
 })()
 
+
+/*
+	Dropdown with the search info
+*/
+function dropdown(){
+	document.getElementById("topic-drop-search").classList.toggle("show");
+}
+
+
+/*
+	Take input from the search bar and update the list shown for topic selection
+	From https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_filter
+*/
+function searchMatch() {
+  var input;
+  var inputUpperCase;
+  var topics;
+  var txtValue;
+  var btn;
+  var i;
+  input = document.getElementById("topic-search");
+  inputUpperCase = input.value.toUpperCase();
+  topics = document.getElementById("topic-drop-search");
+  btn = topics.getElementsByTagName("button");
+  for (i = 0; i < btn.length; i++) {
+    txtValue = btn[i].innerHTML;
+    if (txtValue.toUpperCase().indexOf(inputUpperCase) > -1) {
+      btn[i].style.display = "";
+    } else {
+      btn[i].style.display = "none";
+    }
+  }
+}
 
