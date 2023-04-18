7c1bdd2d8927de5772ae0a2a83a7b06a6123b5cb
jenellemillison <JAMillison@comcast.net>
2023-04-18 07:06:59 -0500

    moving dropdown outside function in js file

--- LSDM/assets/js/java.js
+++ LSDM/assets/js/java.js
@@ -9,38 +9,6 @@
 (function() {
   "use strict";
 	
-	/*
-	Dropdown with the search info
-*/
-function dropdown(){
-	document.getElementById("topic-drop-search").classList.toggle("show");
-}
-
-
-/*
-	Take input from the search bar and update the list shown for topic selection
-	From https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_filter
-*/
-function searchMatch() {
-  var input;
-  var inputUpperCase;
-  var topics;
-  var txtValue;
-  var btn;
-  var i;
-  input = document.getElementById("topic-search");
-  inputUpperCase = input.value.toUpperCase();
-  topics = document.getElementById("topic-drop-search");
-  btn = topics.getElementsByTagName("button");
-  for (i = 0; i < btn.length; i++) {
-    txtValue = btn[i].innerHTML;
-    if (txtValue.toUpperCase().indexOf(inputUpperCase) > -1) {
-      btn[i].style.display = "";
-    } else {
-      btn[i].style.display = "none";
-    }
-  }
-}
 
 
   /**
@@ -268,3 +236,38 @@ function searchMatch() {
   new PureCounter();
 
 })()
+
+
+/*
+	Dropdown with the search info
+*/
+function dropdown(){
+	document.getElementById("topic-drop-search").classList.toggle("show");
+}
+
+
+/*
+	Take input from the search bar and update the list shown for topic selection
+	From https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_filter
+*/
+function searchMatch() {
+  var input;
+  var inputUpperCase;
+  var topics;
+  var txtValue;
+  var btn;
+  var i;
+  input = document.getElementById("topic-search");
+  inputUpperCase = input.value.toUpperCase();
+  topics = document.getElementById("topic-drop-search");
+  btn = topics.getElementsByTagName("button");
+  for (i = 0; i < btn.length; i++) {
+    txtValue = btn[i].innerHTML;
+    if (txtValue.toUpperCase().indexOf(inputUpperCase) > -1) {
+      btn[i].style.display = "";
+    } else {
+      btn[i].style.display = "none";
+    }
+  }
+}
+
