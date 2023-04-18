acf9de26e6497489a06d0a37efdc7c9c035239bd
jenellemillison <JAMillison@comcast.net>
2023-04-18 09:05:26 -0500

    commented out swiper code

--- LSDM/assets/js/java.js
+++ LSDM/assets/js/java.js
@@ -178,37 +178,37 @@
   /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+//  new Swiper('.testimonials-slider', {
+//    speed: 600,
+//    loop: true,
+//    autoplay: {
+//      delay: 5000,
+//      disableOnInteraction: false
+//    },
+//    slidesPerView: 'auto',
+//    pagination: {
+//      el: '.swiper-pagination',
+//      type: 'bullets',
+//      clickable: true
+//    }
+//  });
 
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+//  /**
+//   * Portfolio details slider
+//   */
+//  new Swiper('.portfolio-details-slider', {
+//    speed: 400,
+//    loop: true,
+//    autoplay: {
+//      delay: 5000,
+//      disableOnInteraction: false
+//    },
+//    pagination: {
+//      el: '.swiper-pagination',
+//      type: 'bullets',
+//      clickable: true
+//    }
+//  });
 
   /**
    * Preloader
