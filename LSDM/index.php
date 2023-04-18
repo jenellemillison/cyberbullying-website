dc7aebede916bf32f62597e5e44aa5748c9d2355
jenellemillison <JAMillison@comcast.net>
2023-04-18 06:04:41 -0500

    put back id=navbar in nav tag

--- LSDM/index.php
+++ LSDM/index.php
@@ -11,7 +11,7 @@ echo '<html lang="en">
 <body>
 <header id="header" class="fixed-top">
   <div class="container d-flex align-items-center justify-content-between">
-        <nav class="navbar">
+        <nav id="navbar" class="navbar">
           <ul>
             <li><a class="nav-link scrollto active" href="#hero">Home</a> </li>
             <li><a class="nav-link scrollto" href="#about">About</a> </li>
