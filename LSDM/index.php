f39d20c0f69c214a80ff04407d18603ca65e7913
jenellemillison <JAMillison@comcast.net>
2023-04-18 06:33:27 -0500

    include css files and change order of css imports

--- LSDM/index.php
+++ LSDM/index.php
@@ -5,8 +5,10 @@ echo '<html lang="en">
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <!-- CSS FILES -->
-<link rel="stylesheet" href="assets/css/style2.css">
 <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/style2.css">
 </head>
 <body>
 <header id="header" class="fixed-top">
