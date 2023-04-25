946f4d46f2c3f38a5166e9d680580d9492ba8b5c
jenellemillison <JAMillison@comcast.net>
2023-04-24 19:54:34 -0500

    LIMIT instead of TOP

--- LSDM/queryDB.php
+++ LSDM/queryDB.php
@@ -7,7 +7,7 @@ $password = $_ENV["MYSQLPASSWORD"];
 $dbname = $_ENV["MYSQLDATABASE"];
 // Create new mysql connection
 $dblink = new mysqli( $hostname, $username, $password, $db, $port); //make the connection to the db
-$top10sql = "Select top 10 * from Tweets";
+$top10sql = "Select TOP 10 * from Tweets";
 $top10queryresults = $dblink->query( $top10sql )or die( "<p>Something went wrong with: $top10sql<br>". $dblink->error ); //execute the above query or call the error class with dblink
 
 while ( $socialdata = $top10queryresults->fetch_array( MYSQLI_ASSOC ) ) { //grab all from array and give it as an associative array
