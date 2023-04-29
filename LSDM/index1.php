d5c0f3902df5a0e062e489a75f2107bc09a8a541
jenellemillison <JAMillison@comcast.net>
2023-04-29 18:30:47 -0500

    give more time to the site

--- LSDM/index1.php
+++ LSDM/index1.php
@@ -493,7 +493,7 @@ $dbname = $_ENV[ "MYSQLDATABASE" ];
 			}
 		});
 	};
-	setInterval(function(){refresh_div();}, 10000)
+	setInterval(function(){refresh_div();}, 50000)
 
 //	window.onload = function(){
 //		var reloading = sessionStorage.getItem("reloading");
