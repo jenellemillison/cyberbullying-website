356fdbaabe331d14f33cca138561dd0dc55ebb42
jenellemillison <JAMillison@comcast.net>
2023-04-30 05:48:38 -0500

    updated stylesheet for bootstrap icons

--- LSDM/index.php
+++ LSDM/index.php
@@ -7,7 +7,7 @@ echo '<html lang="en">
 <!-- CSS FILES -->
 <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="assets/css/style.css">
-<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 <!-- JS FILES -->
 <script src="assets/js/jquery-3.5.1.js"></script>
 </head>
