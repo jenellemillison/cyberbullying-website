4b9197a0508000f2949983e928922b3c407d63d2
jenellemillison <JAMillison@comcast.net>
2023-04-30 03:21:01 -0500

    add a per page to the limit so it's per page, bottom, top

--- LSDM/index1.php
+++ LSDM/index1.php
@@ -186,7 +186,7 @@ echo '<html lang="en">
 			// Create new mysql connection
 			$dblink = new mysqli( $servername, $username, $password, $dbname, $port); //make the connection to the db
 			//echo "\"SELECT * from Tweets WHERE topic LIKE \"%". $topic ."%\" ORDER BY auto_id DESC LIMIT 10;\"";
-			$top10sql = "SELECT * from Tweets WHERE topic LIKE \"%". $topic ."%\" ORDER BY auto_id DESC LIMIT " . $bottom_limit . " , " . $top_limit . ";";
+			$top10sql = "SELECT * from Tweets WHERE topic LIKE \"%". $topic ."%\" ORDER BY auto_id DESC LIMIT " . $perPage. " , " . $bottom_limit . " , " . $top_limit . ";";
 			$top10queryresults = $dblink->query( $top10sql )or die( "<p>Something went wrong with: $top10sql<br>". $dblink->error ); //execute the above query or call the error class with dblink
 			while ( $socialdata = $top10queryresults->fetch_array( MYSQLI_ASSOC ) ) { //grab all from array and give it as an associative array
 			  echo '<tr>';
