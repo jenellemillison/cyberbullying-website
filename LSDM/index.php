843e49f628682cf53c409af0e22402271e1b9b92
Timothy-Hill14 <thrill321@gmail.com>
2023-04-30 02:59:13 -0500

    update title

--- LSDM/index.php
+++ LSDM/index.php
@@ -192,7 +192,7 @@ echo '<html lang="en">
         <div class="service-box">
           <div class="service-ico"> <span class="ico-circle"><i class="bi bi-briefcase"></i></span> </div>
           <div class="service-content">
-            <h2 class="s-title">Web Design</h2>
+            <h2 class="s-title">. $socialdata["username"] .</h2>
             <p class="s-description text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia,
               provident vitae! Magni
               tempora perferendis eum non provident. </p>
