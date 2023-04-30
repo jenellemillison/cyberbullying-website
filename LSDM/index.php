54f53dd6adddca20f4aa19a6d11a7fae518543f2
Timothy-Hill14 <thrill321@gmail.com>
2023-04-30 03:22:02 -0500

    updated changes

--- LSDM/index.php
+++ LSDM/index.php
@@ -173,16 +173,16 @@ echo '<html lang="en">
 			//echo "\"SELECT * from Tweets WHERE topic LIKE \"%". $topic ."%\" ORDER BY auto_id DESC LIMIT 10;\"";
 			$top10sql = "SELECT * from Tweets WHERE topic LIKE \"%". $topic ."%\" ORDER BY auto_id DESC LIMIT 10;";
 			$top10queryresults = $dblink->query( $top10sql )or die( "<p>Something went wrong with: $top10sql<br>". $dblink->error ); //execute the above query or call the error class with dblink
-			while ( $socialdata = $top10queryresults->fetch_array( MYSQLI_ASSOC ) ) { //grab all from array and give it as an associative array
-			  echo '<tr>';
-			  echo '<td>' . $socialdata['username'] . '</td>';
-			  echo '<td>' . $socialdata['text'] . '</td>';
-			  echo '<td>' . $socialdata['time_posted'] . '</td>';
-			  echo '<td>' . $socialdata['cyberbullying_category'] . '</td>';
-			  echo '</tr>';
+		//	while ( $socialdata = $top10queryresults->fetch_array( MYSQLI_ASSOC ) ) { //grab all from array and give it as an associative array
+		//	  echo '<tr>';
+		//	  echo '<td>' . $socialdata['username'] . '</td>';
+		//	  echo '<td>' . $socialdata['text'] . '</td>';
+		//	  echo '<td>' . $socialdata['time_posted'] . '</td>';
+		//	  echo '<td>' . $socialdata['cyberbullying_category'] . '</td>';
+		//	  echo '</tr>';
 			//}
 //			echo '</tbody>';
-			echo '</table>';
+	//		echo '</table>';
 			echo '<div class="line-mf"></div>';
        echo' </div>';
       echo '</div>';
@@ -192,6 +192,7 @@ echo '<html lang="en">
         echo'<div class="service-box">';
          echo' <div class="service-ico"> <span class="ico-circle"><i class="bi bi-briefcase"></i></span> </div>';
           echo'<div class="service-content">';
+		while ( $socialdata = $top10queryresults->fetch_array( MYSQLI_ASSOC ) ) { 
 		 echo' <h2 class="s-title">' . $socialdata["username"] . '</h2>';
           echo'  <p class="s-description text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia,
               provident vitae! Magni
