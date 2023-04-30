908cdacad27803c97f329c74eedf6e6608d99f68
jenellemillison <JAMillison@comcast.net>
2023-04-30 06:19:24 -0500

    don't print query, print number

--- LSDM/index.php
+++ LSDM/index.php
@@ -223,7 +223,7 @@ echo '<html lang="en">
   ';
   $numGenderBullyQuery = "SELECT COUNT(cyberbullying_category) from Tweets WHERE topic LIKE \"%". $topic ."%\" AND cyberbullying_category LIKE \"gender\";";
   $numGenderyBully = $dblink->query( $numGenderBullyQuery )or die( "<p>Something went wrong with: $numGenderBullyQuery<br>". $dblink->error ); //execute the above query or call the error class with dblink
-  echo '<h1>' . $numGenderBullyQuery .'</h1>';
+  echo '<h1>' . $numGenderBully .'</h1>';
   
   
       '<!-- ======= Counter Section ======= -->
